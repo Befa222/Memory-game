581ad93ee2b7544e9bb3cc7a2fff49ad017019b3
Befa222 <72620186+Befa222@users.noreply.github.com>
2021-11-03 11:26:42 +0000

    added file in gitignore

--- src/firebase.js
+++ src/firebase.js
@@ -4,14 +4,15 @@ import {getDatabase} from 'firebase/database'
 
 
 
+
 const app = {
-    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-    databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
-    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_FIREBASE_APP_ID
+    apiKey: "AIzaSyClYR61pWZ4vyP4dRB16MXv5n55BxEC1Gw",
+  authDomain: "memory-game-7f6eb.firebaseapp.com",
+  databaseURL: "https://memory-game-7f6eb-default-rtdb.firebaseio.com",
+  projectId: "memory-game-7f6eb",
+  storageBucket: "memory-game-7f6eb.appspot.com",
+  messagingSenderId: "747326177648",
+  appId: "1:747326177648:web:aea5b7e0151d2287550fb5"
 
 }
 const config = initializeApp(app)
